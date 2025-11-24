5b4d322c474b12db36f234f1ad15825a241c9c0a
Nick Seman <me@vphoebe.dev>
2025-11-24 15:17:16 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# rominfo
+A Python utility for generating hashes for ROM files, specifically for No-Intro submissions.
---
+++ rominfo.py
@@ -0,0 +1,69 @@
+import sys
+import zlib
+import hashlib
+
+
+def get_size(file: bytes):
+    return len(file)
+
+
+def get_crc32(file: bytes):
+    val = zlib.crc32(file)
+    return hex(val)[2:]
+
+
+def get_hashlib_hex(file: bytes, m: hashlib._Hash):
+    m.update(file)
+    return m.hexdigest()
+
+
+def get_md5(file: bytes):
+    m = hashlib.md5()
+    return get_hashlib_hex(file, m)
+
+
+def get_sha1(file: bytes):
+    m = hashlib.sha1()
+    return get_hashlib_hex(file, m)
+
+
+def get_sha256(file: bytes):
+    m = hashlib.sha256()
+    return get_hashlib_hex(file, m)
+
+
+def get_file_string(file_path: str):
+    with open(file_path, 'rb') as f:
+        file = f.read()
+        file_name = file_path.split('/').pop()
+        return f"""--- {file_name} ---
+Size: {get_size(file)} bytes
+CRC32: {get_crc32(file)}
+MD5: {get_md5(file)}
+SHA-1: {get_sha1(file)}
+SHA-256: {get_sha256(file)}\n\n"""
+
+
+def read_file_args():
+    if len(sys.argv) < 2:
+        print(
+            "Error: No files provided.\nUsage: python rominfo.py file1.rom [file2.rom ...]")
+        sys.exit(1)
+
+    strings = []
+    for file_path in sys.argv[1:]:
+        try:
+            string = get_file_string(file_path)
+            strings.append(string)
+        except FileNotFoundError:
+            print(f"Error: File '{file_path}' not found.")
+        except Exception as e:
+            print(f"Error reading '{file_path}': {str(e)}")
+
+    with open('hashes.txt', 'w') as output:
+        output.write(''.join(strings))
+        print(f"Wrote {len(strings)} file(s) to hashes.txt")
+
+
+if __name__ == "__main__":
+    sys.exit(read_file_args())
